f804ddb4f9354de0dcc7876b0ba7d4c41e09de30
Mohammad FS <mohammad.fs243@gmail.com>
2025-03-10 21:34:44 +0330

    Set options

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
+-- for easier and more intuitive setting options
+local set = vim.opt
+
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>x", ":.lua<CR>")
 vim.keymap.set("v", "<space>x", ":lua<CR>")
@@ -6,6 +9,10 @@ vim.keymap.set("n", "<tab>", ":bnext<CR>")
 vim.keymap.set("n", "<S-tab>", ":bprevious<CR>")
 
 vim.keymap.set("n", "<space>pv", ":Exp<CR>")
+
+set.shiftwidth = 4
+set.number = true
+set.relativenumber = true
 
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
