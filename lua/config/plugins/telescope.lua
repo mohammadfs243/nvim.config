fac9b660c7d73e141d80f97f0355cead659adeaf
Mohammad FS <mohammad.fs243@gmail.com>
2025-03-11 14:26:04 +0330

    Install telescope

---
+++ lua/config/plugins/telescope.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" }
+    },
+    config = function()
+      require("telescope").setup {
+        -- pickers = {
+        --   help_tags = {
+        --     theme = "dropdown"
+        --   }
+        -- },
+      }
+      vim.keymap.set("n", "<space>ff", require("telescope.builtin").find_files)
+      vim.keymap.set("n", "<space>fh", require("telescope.builtin").help_tags)
+      vim.keymap.set("n", "<space>tb", function()
+        local opts = require("telescope.themes").get_ivy()
+        require("telescope.builtin").builtin(opts)
+      end)
+      vim.keymap.set("n", "<space>en", function()
+        require("telescope.builtin").find_files {
+          cwd = vim.fn.stdpath("config")
+        }
+      end)
+    end
+  }
+}
