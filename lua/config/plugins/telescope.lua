ed13547bf395204bbc547ade3a1bbad006fc42ff
Mohammad FS <mohammad.fs243@gmail.com>
2025-03-11 22:52:42 +0330

    Add example of setting up defautls for telescope

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -13,6 +13,16 @@ return {
         --     theme = "dropdown"
         --   }
         -- },
+        defaults = {
+          mappings = {
+            i = {
+              -- map actions.which_key to <C-h> (default: <C-/>)
+              -- actions.which_key shows the mappings for your picker,
+              -- e.g. git_{create, delete, ...}_branch for the git_branches picker
+              ["<C-h>"] = "which_key"
+            }
+          }
+        },
         extensions = {
           fzf = {}
         }
