2b30b618f91e5dd62f5c701b36b4f14f220c8c43
Mohammad FS <mohammad.fs243@gmail.com>
2025-03-22 15:35:25 +0330

    Set up autoformat

--- init.lua
+++ init.lua
@@ -15,6 +15,7 @@ vim.keymap.set("t", "<C-q>", "<C-\\><C-n>")
 
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.opt.softtabstop = 4
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "yes"
@@ -22,9 +23,9 @@ vim.opt.signcolumn = "yes"
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
   callback = function()
     vim.highlight.on_yank()
   end,
